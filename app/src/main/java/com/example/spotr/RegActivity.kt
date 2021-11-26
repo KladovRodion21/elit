be4c02c24834962feb193311da3264269ca77938
kladov <kladov_1999@inbox.ru>
2021-11-26 21:01:18 +0500

    1

--- app/src/main/java/com/example/spotr/RegActivity.kt
+++ app/src/main/java/com/example/spotr/RegActivity.kt
@@ -1,11 +1,34 @@
 package com.example.spotr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class RegActivity : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var repeatpass: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg)
+        password= findViewById(R.id.editTextTextPasswordreg)
+        email= findViewById(R.id.editTextTextEmailAddressReg)
+        repeatpass= findViewById(R.id.editTextTextRepeatpass)
+    }
+    fun registrations (view:android.view.View){
+        if(email.text.isNotEmpty() && password.text.isNotEmpty()&&repeatpass.text.isNotEmpty()&&repeatpass==password && Patterns.EMAIL_ADDRESS.matcher(email.text).matches())
+        {
+            startActivity(Intent(this@RegActivity, SignInActivity::class.java))
+
+        }
+        else AlertDialog.Builder(this@RegActivity).setMessage("Неверные данные регистрации").show()
+    }
+    fun back(view:android.view.View){
+        startActivity(Intent(this@RegActivity, SignInActivity::class.java))
     }
 }
--- app/src/main/java/com/example/spotr/SignInActivity.kt
+++ app/src/main/java/com/example/spotr/SignInActivity.kt
@@ -3,11 +3,18 @@ package com.example.spotr
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.create
+import java.util.regex.Pattern.compile
 
 class SignInActivity : AppCompatActivity() {
     lateinit var email: EditText
@@ -26,9 +33,28 @@ class SignInActivity : AppCompatActivity() {
     }
 
 
+
     fun Login (view:android.view.View){
-        if(email.text.isNotEmpty() && password.text.isNotEmpty())
+        if(email.text.isNotEmpty() && password.text.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email.text).matches())
         {
+            val retrofit =MyRetrofit().getRetrofit().create(RetApi::class.java)
+            val hashMap: HashMap<String, String> = HashMap()
+            hashMap["email"] = email.text.toString()
+            hashMap["password"] = password.text.toString()
+            val call: Call<Login> = retrofit.login(hashMap)
+            call.enqueue((object: retrofit2.Callback<Login>{
+                override fun OnResponse(call: Call<Login>, response: Response<Login>){
+                    if (response.body()?.token!=null){
+                        Login.userToken = response.body()?.token
+                        Toast.makeText(this@SignInActivity,"Успешно", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this@SignInActivity,MainActivity::class.java))
+                        finish()
+                    }
+                    else{
+
+                    }
+                }
+            }))
             startActivity(Intent(this@SignInActivity, MainActivity::class.java))
 
         }
